c41f3af9bc562dafbf3b3c7f8b5dd61510ad214b
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-03-08 15:53:32 -0300

    fooooooooo

--- .config/nvim/lua/plugins/code-features/blink-cmp.lua
+++ .config/nvim/lua/plugins/code-features/blink-cmp.lua
@@ -1,55 +1,65 @@
 return {
-  'saghen/blink.cmp',
+	"saghen/blink.cmp",
 
-  dependencies = {
-    'rafamadriz/friendly-snippets',
-    'iguanacucumber/magazine.nvim',
-  },
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+		"iguanacucumber/magazine.nvim",
+	},
 
-  version = 'v0.*',
+	version = "v0.*",
 
-  event = "VeryLazy",
+	event = "VeryLazy",
 
-  opts = {
-    keymap = {
-      ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-      ['<C-e>'] = { 'hide', 'fallback' },
-      ['<CR>'] = { 'accept', 'fallback' },
+	opts = {
+		keymap = {
+			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+			["<C-e>"] = { "hide", "fallback" },
+			["<CR>"] = { "accept", "fallback" },
 
-      ['<Tab>'] = { 'snippet_forward', 'fallback' },
-      ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
-      ['<C-j>'] = { 'snippet_forward', 'fallback' },
-      ['<C-k>'] = { 'snippet_backward', 'fallback' },
+			["<Tab>"] = { "snippet_forward", "fallback" },
+			["<S-Tab>"] = { "snippet_backward", "fallback" },
+			["<C-j>"] = { "snippet_forward", "fallback" },
+			["<C-k>"] = { "snippet_backward", "fallback" },
 
-      ['<C-p>'] = { 'select_prev', 'fallback' },
-      ['<C-o>'] = { 'select_next', 'fallback' },
+			["<C-p>"] = { "select_prev", "fallback" },
+			["<C-o>"] = { "select_next", "fallback" },
 
-      ['<Up>'] = { 'scroll_documentation_up', 'fallback' },
-      ['<Down>'] = { 'scroll_documentation_down', 'fallback' },
-    },
+			["<Up>"] = { "scroll_documentation_up", "fallback" },
+			["<Down>"] = { "scroll_documentation_down", "fallback" },
+		},
 
-    appearance = {
-      use_nvim_cmp_as_default = true,
-      nerd_font_variant = "normal"
-    },
+		cmdline = {
+			keymap = {
+				["<C-e>"] = { "hide", "fallback" },
+				["<CR>"] = { "accept", "fallback" },
 
-    completion = { documentation = {
-        auto_show = true,
-        auto_show_delay_ms = 0,
+				["<C-p>"] = { "select_prev", "fallback" },
+				["<C-o>"] = { "select_next", "fallback" },
+			},
+		},
 
-        window = {
-          border = "single"
-        }
-      },
+		appearance = {
+			use_nvim_cmp_as_default = true,
+			nerd_font_variant = "normal",
+		},
 
-      trigger = {
-        show_on_x_blocked_trigger_characters = { "'", '"', "(", "{", "`", "[" },
-      },
-    },
+		completion = {
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 0,
 
-    signature = {
-      enabled = true,
-    },
+				window = {
+					border = "single",
+				},
+			},
 
-  }
+			trigger = {
+				show_on_x_blocked_trigger_characters = { "'", '"', "(", "{", "`", "[" },
+			},
+		},
+
+		signature = {
+			enabled = true,
+		},
+	},
 }
